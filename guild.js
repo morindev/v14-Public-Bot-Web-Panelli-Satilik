b56296650a9fa31b06e881a69180ed7cc590720b
luhux <74924310+utw0@users.noreply.github.com>
2024-06-14 12:21:34 +0300

    Create guild.js

---
+++ guild.js
@@ -0,0 +1,121 @@
+module.exports = {
+    emojiler: [
+        { name: "appEmoji_bir", link: "https://cdn.discordapp.com/emojis/994628089592156220.webp?size=96&quality=lossless" },
+        { name: "appEmoji_iki", link: "https://cdn.discordapp.com/emojis/994628094172332062.webp?size=96&quality=lossless" },
+        { name: "appEmoji_uc", link: "https://cdn.discordapp.com/emojis/994628099738189894.webp?size=96&quality=lossless" },
+        { name: "appEmoji_dort", link: "https://cdn.discordapp.com/emojis/994628103496286228.webp?size=96&quality=lossless" },
+        { name: "appEmoji_bes", link: "https://cdn.discordapp.com/emojis/994628107443122246.webp?size=96&quality=lossless" },
+        { name: "appEmoji_alti", link: "https://cdn.discordapp.com/emojis/994628111771652248.webp?size=96&quality=lossless" },
+        { name: "appEmoji_yedi", link: "https://cdn.discordapp.com/emojis/994628116603469844.webp?size=96&quality=lossless" },
+        { name: "appEmoji_sekiz", link: "https://cdn.discordapp.com/emojis/994628120789393458.webp?size=96&quality=lossless" },
+        { name: "appEmoji_dokuz", link: "https://cdn.discordapp.com/emojis/994628124782379088.webp?size=96&quality=lossless" },
+        { name: "appEmoji_sifir", link: "https://cdn.discordapp.com/emojis/994628084709994536.webp?size=96&quality=lossless" },
+        { name: "appEmoji_cop", link: "https://cdn.discordapp.com/emojis/994706622536503367.webp?size=96&quality=lossless" },
+        { name: "appEmoji_cark", link: "https://cdn.discordapp.com/emojis/995772653166137444.webp?size=96&quality=lossless" },
+        { name: "appEmoji_bot", link: "https://cdn.discordapp.com/emojis/994706632225337427.webp?size=96&quality=lossless" },
+        { name: "appEmoji_saat", link: "https://cdn.discordapp.com/emojis/995063032134770808.webp?size=96&quality=lossless" },
+        { name: "appEmoji_solOk", link: "https://cdn.discordapp.com/emojis/995056594805076029.webp?size=96&quality=lossless" },
+        { name: "appEmoji_sagOk", link: "https://cdn.discordapp.com/emojis/995056603025916034.webp?size=96&quality=lossless" },
+        { name: "appEmoji_tik", link: "https://cdn.discordapp.com/emojis/992914567334211655.webp?size=96&quality=lossless" },
+        { name: "appEmoji_carpi", link: "https://cdn.discordapp.com/emojis/992914543724466268.webp?size=96&quality=lossless" },
+        { name: "appEmoji_ses", link: "https://cdn.discordapp.com/emojis/1059113666122878976.webp?size=96&quality=lossless" },
+        { name: "appEmoji_metin", link: "https://cdn.discordapp.com/emojis/994706608162615416.webp?size=96&quality=lossless" },
+        { name: "appEmoji_kamera", link: "https://cdn.discordapp.com/emojis/992914542029963315.webp?size=96&quality=lossless" },
+        { name: "appEmoji_yayin", link: "https://cdn.discordapp.com/emojis/994626651277246484.webp?size=96&quality=lossless" },
+        { name: "appEmoji_oynat", link: "https://cdn.discordapp.com/emojis/993985938277539991.webp?size=96&quality=lossless" },
+        { name: "appEmoji_elmas", link: "https://cdn.discordapp.com/emojis/995772655766622259.webp?size=96&quality=lossless" },
+        { name: "appEmoji_kategori", link: "https://cdn.discordapp.com/emojis/992914549118337167.webp?size=96&quality=lossless" },
+        { name: "appEmoji_davet", link: "https://cdn.discordapp.com/emojis/1061356516403318845.webp?size=96&quality=lossless" },
+        { name: "appEmoji_coin", link: "https://cdn.discordapp.com/emojis/1061357707023626240.webp?size=96&quality=lossless" },
+        { name: "appEmoji_yukarcizgi", link: "https://cdn.discordapp.com/emojis/1062288419016933446.webp?size=44&quality=lossless" },
+        { name: "appEmoji_boskutu", link: "https://cdn.discordapp.com/emojis/1062288417003671673.webp?size=44&quality=lossless" },
+        { name: "appEmoji_kalp", link: "https://cdn.discordapp.com/emojis/1062291976239714376.webp?size=44&quality=lossless" },
+        { name: "appEmoji_kiraz", link: "https://cdn.discordapp.com/emojis/1062291978378813460.webp?size=44&quality=lossless" },
+        { name: "appEmoji_patlican", link: "https://cdn.discordapp.com/emojis/1062291981893648424.webp?size=44&quality=lossless" },
+        { name: "appEmoji_slot", link: "https://cdn.discordapp.com/emojis/1062288778053570660.gif?size=96&quality=lossless" },
+        { name: "appEmoji_web", link: "https://cdn.discordapp.com/emojis/1062691786369286144.webp?size=96&quality=lossless" },
+        { name: "appEmoji_bilgisayar", link: "https://cdn.discordapp.com/emojis/1062691585927696434.webp?size=96&quality=lossless" },
+        { name: "appEmoji_telefon", link: "https://cdn.discordapp.com/emojis/1062691582807113769.webp?size=96&quality=lossless" },
+        { name: "appEmoji_level", link: "https://cdn.discordapp.com/emojis/1063762712078864474.webp?size=96&quality=lossless" },
+        { name: "appEmoji_kup", link: "https://cdn.discordapp.com/emojis/1062774380708503572.webp?size=96&quality=lossless" },
+        { name: "appEmoji_unlem", link: "https://cdn.discordapp.com/emojis/993229107968094298.webp?size=96&quality=lossless" },
+        { name: "appEmoji_guard", link: "https://cdn.discordapp.com/emojis/1064986417115172995.webp?size=96&quality=lossless" },
+        { name: "nokta2", link: "https://cdn.discordapp.com/emojis/1068524729578823710.webp?size=96&quality=lossless" },
+        { name: "Kirmizi", link: "https://cdn.discordapp.com/emojis/1100090235079114944.webp?size=96&quality=lossless" },
+        { name: "appEmoji_ceza", link: "https://cdn.discordapp.com/emojis/1102563504923353139.webp?size=96&quality=lossless" },
+        { name: "appEmoji_obg", link: "https://cdn.discordapp.com/emojis/1247918968715346031.webp?size=96&quality=lossless" },
+        { name: "appEmoji_bbg", link: "https://cdn.discordapp.com/emojis/1247918939489439788.webp?size=96&quality=lossless" },
+        { name: "appEmoji_sbg", link: "https://cdn.discordapp.com/emojis/1247918997874147381.webp?size=96&quality=lossless" },
+        { name: "appEmoji_bby", link: "https://cdn.discordapp.com/emojis/1247919028060815360.webp?size=96&quality=lossless" },
+        { name: "appEmoji_oby", link: "https://cdn.discordapp.com/emojis/1247919057496178708.webp?size=96&quality=lossless" },
+        { name: "appEmoji_sby", link: "https://cdn.discordapp.com/emojis/1247919083547004958.webp?size=96&quality=lossless" },
+
+    ],
+    emojiSpecalRoomEmojis: [
+        { name: "appEmoji_create", link: "https://cdn.discordapp.com/emojis/1118234416121122917.webp?size=96&quality=lossless" },
+        { name: "appEmoji_duzenle", link: "https://cdn.discordapp.com/emojis/1117696605563387975.webp?size=96&quality=lossless" },
+        { name: "appEmoji_ekle", link: "https://cdn.discordapp.com/emojis/1117696607475994646.webp?size=96&quality=lossless" },
+        { name: "appEmoji_cikar", link: "https://cdn.discordapp.com/emojis/1117696601633333319.webp?size=96&quality=lossless" },
+        { name: "appEmoji_gorunmez", link: "https://cdn.discordapp.com/emojis/1117696609766092941.webp?size=96&quality=lossless" },
+        { name: "appEmoji_gorunur", link: "https://cdn.discordapp.com/emojis/1117696613318672464.webp?size=96&quality=lossless" },
+        { name: "appEmoji_kilidac", link: "https://cdn.discordapp.com/emojis/1117696615298371664.webp?size=96&quality=lossless" },
+        { name: "appEmoji_kilitkapat", link: "https://cdn.discordapp.com/emojis/1117696617315831892.webp?size=96&quality=lossless" },
+        { name: "appEmoji_cop", link: "https://cdn.discordapp.com/emojis/1117696603466244096.webp?size=96&quality=lossless" },
+    ],
+
+    loglar: ["yetki-atlama-log", "mazaret-log", "streamer-başvuru-log", "yetkili-başvuru-log", "sorumluluk_görev_log", "server-support", "stats-log", "join-log", "leave-log", "level-log", "kayıtsız_log", "kayıt_log", "yasaklıtag_log", "görev-log", "audit-log", "message_log", "voicejoin_log", "voiceswitch_log", "voiceleave_log", "streamingstart_log", "streamingstop_log", "roleadd_log", "roleremove_log", "displayname_log", "camerastart_log", "camerastop_log"],
+    gloglar: ["web-guard", "offline-control", "emojisticker-guard", "server-guard", "bankick-guard", "channel-guard", "role-guard",],
+    Guild: {
+        Bots: {
+            mainToken: "",
+            statToken: "",
+            ProsecutorToken: "",
+            guardOne: "",
+            guardTwo: "",
+            guardThree: "",
+            MongoDb: "",
+            prefixs: ["."],
+            devs: ["341592492224806914"],
+            Dis: [],
+            selfToken: "",
+            voices: [
+                { token: "", channelId: "", soundURL: "https://www.youtube.com/watch?v=eVTXPUF4Oz4" },
+            ]
+        },
+        ID: "",
+        vanityURL: "",
+        botVoiceChannel: "",
+        botStatus: "luhux ❤️ ",
+        urlSpammer: "",
+        UrlWebHook: "",
+        StaffAutoRank: {
+            1: { Role: "1249305356690133003", Powers: ["1249305368727781386"], AMP: 200, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            2: { Role: "1249305355641552988", Powers: ["1249305368727781386"], AMP: 300, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            3: { Role: "1249305354530062358", Powers: ["1249305368727781386"], AMP: 500, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            4: { Role: "1249305353536143410", Powers: ["1249305368727781386"], AMP: 700, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            5: { Role: "1249305352491765830", Powers: ["1249305368727781386"], AMP: 800, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+
+            6: { Role: "1249305350243356682", Powers: ["1249305368727781386"], AMP: 900, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            7: { Role: "1249305347479568450", Powers: ["1249305368727781386"], AMP: 1100, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            8: { Role: "1249305346736918619", Powers: ["1249305368727781386"], AMP: 1300, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            9: { Role: "1249305345730547713", Powers: ["1249305368727781386"], AMP: 1500, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+
+            10: { Role: "1249305343033610340", Powers: ["1249305368727781386", "1249305344824315945"], AMP: 1700, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            11: { Role: "1249305341515268139", Powers: ["1249305368727781386", "1249305344824315945"], AMP: 1900, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            12: { Role: "1249305340873408603", Powers: ["1249305368727781386", "1249305344824315945"], AMP: 2100, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            13: { Role: "1249305339820642368", Powers: ["1249305368727781386", "1249305344824315945"], AMP: 2400, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+
+            14: { Role: "1249305336821579810", Powers: ["1249305368727781386", "1249305338952290344"], AMP: 2600, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            15: { Role: "1249305336150622269", Powers: ["1249305368727781386", "1249305338952290344"], AMP: 2800, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            16: { Role: "1249305334619574276", Powers: ["1249305368727781386", "1249305338952290344"], AMP: 3100, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+            17: { Role: "1249305333789360128", Powers: ["1249305368727781386", "1249305338952290344"], AMP: 3300, Missions: { V: 3600000, M: 100, R: 10, I: 10, TI: 5, SI: 3 } },
+        
+
+
+
+
+
+        }
+    }
+
+};
